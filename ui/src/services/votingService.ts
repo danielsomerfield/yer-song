73279cc1301f2567181cd4a0bb40a8f8bf307ca1
Daniel Somerfield <daniel.somerfield@hatchdata.com>
2023-10-29 15:06:20 -0700

    Fix bug with URL path

--- ui/src/services/votingService.ts
+++ ui/src/services/votingService.ts
@@ -6,9 +6,7 @@ export const createVoteForSong = (
   httpClient: Axios = axios,
 ) => {
   return async (songId: string): Promise<void> => {
-    const url = `${
-      configuration.songsAPIHostURL
-    }/vote/songs/${encodeURIComponent(songId)}`;
+    const url = `${configuration.songsAPIHostURL}/vote/songs/${songId}`;
     const response = await httpClient.post(url);
     if (response.status != 200) {
       console.log("Vote failed with error code", response.status);
